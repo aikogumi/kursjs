3335017bd4af37eef38ae2f027635e076664481c
Agata Zawiłowicz <aiko.gumi@gmail.com>
2017-04-16 23:31:37 +0200

    wpisywanie licz w ciągu

---
+++ kalkulator.js
@@ -0,0 +1,41 @@
+var dzialanie="";
+function dziewienc() {
+	dzialanie = dzialanie + "9";
+		document.getElementById("wynik").innerHTML = dzialanie;
+	}
+function osiem() {
+	dzialanie = dzialanie + "8"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function siedem() {
+	dzialanie = dzialanie + "7"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function szesc() {
+	dzialanie = dzialanie + "6"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function pinc() {
+	dzialanie= dzialanie + "5"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function cztery() {
+	dzialanie= dzialanie + "4"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function trzy() {
+	dzialanie = dzialanie + "3"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function dwa() {
+	dzialanie = dzialanie + "2"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function jeden() {
+	dzialanie = dzialanie + "1"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function zero() {
+	dzialanie = dzialanie + "0"
+	document.getElementById("wynik").innerHTML = dzialanie;
+}

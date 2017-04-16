9e8c6ce690dfed66d2d84e880d781d474516f34a
Agata Zawiłowicz <aiko.gumi@gmail.com>
2017-04-16 23:47:27 +0200

    dużo rzeczy

--- kalkulator.js
+++ kalkulator.js
@@ -4,38 +4,60 @@ function dziewienc() {
 		document.getElementById("wynik").innerHTML = dzialanie;
 	}
 function osiem() {
-	dzialanie = dzialanie + "8"
+	dzialanie = dzialanie + "8";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function siedem() {
-	dzialanie = dzialanie + "7"
+	dzialanie = dzialanie + "7";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function szesc() {
-	dzialanie = dzialanie + "6"
+	dzialanie = dzialanie + "6";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function pinc() {
-	dzialanie= dzialanie + "5"
+	dzialanie= dzialanie + "5";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function cztery() {
-	dzialanie= dzialanie + "4"
+	dzialanie= dzialanie + "4";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function trzy() {
-	dzialanie = dzialanie + "3"
+	dzialanie = dzialanie + "3";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function dwa() {
-	dzialanie = dzialanie + "2"
+	dzialanie = dzialanie + "2";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function jeden() {
-	dzialanie = dzialanie + "1"
+	dzialanie = dzialanie + "1";
 	document.getElementById("wynik").innerHTML = dzialanie;
 }
 function zero() {
-	dzialanie = dzialanie + "0"
+	dzialanie = dzialanie + "0";
 	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function dodawanie() {
+	dzialanie = dzialanie + "+";
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function odejmowanie() {
+	dzialanie = dzialanie + "-";
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function dzielenie() {
+	dzialanie = dzialanie + "/";
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function mnozenie() {
+	dzialanie = dzialanie + "*";
+	document.getElementById("wynik").innerHTML = dzialanie;
+}
+function wynik() {
+	if (dzialanie.indexOf("+") !==-1){
+		console.log("ncoaivw")
+		
+	}
 }
